ca65d3177578b498fd092374426f8d4c6114f6c5
Jesus Conde <educaleft@gmail.com>
2021-12-06 13:23:34 +0000

    Agregar archivos de proyecto.

---
+++ Bloques/Bloques.cpp
@@ -0,0 +1,15 @@
+int sumar(int x, int y)
+{ // comienzo bloque
+    return x + y;
+} // final bloque (no necesita punto y coma)
+
+int main()
+{ // comienzo bloque
+
+    // múltiples declaraciones
+    int value{}; // esto es inicialización, no un bloque
+    sumar(3, 4);
+
+    return 0;
+
+} // final bloque (sin punto y coma)

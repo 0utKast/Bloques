2ba71ac51647450148532b47080bd12bf5f07b2c
Jesus Conde <educaleft@gmail.com>
2021-12-06 13:29:04 +0000

    Añadido ejemplo de bloque anidado.

--- Bloques/Bloques.cpp
+++ Bloques/Bloques.cpp
@@ -1,4 +1,34 @@
 int sumar(int x, int y)
+{ // inicio bloque
+    return x + y;
+} // final bloque
+
+int main()
+{ // otro bloque
+
+    // múltiples declaraciones
+    int valor{};
+
+    { // bloque interior/anidado
+        sumar(3, 4);
+    } // fin bloque interior/anidado
+
+    return 0;
+
+} // final bloque exterior
+
+
+
+
+
+
+
+
+
+
+
+
+/*int sumar(int x, int y)
 { // comienzo bloque
     return x + y;
 } // final bloque (no necesita punto y coma)
@@ -12,4 +42,4 @@ int main()
 
     return 0;
 
-} // final bloque (sin punto y coma)
+} // final bloque (sin punto y coma)*/

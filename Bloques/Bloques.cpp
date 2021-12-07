1bc50b757c36b904e41f4a133f9daed4e32aba6e
Jesus Conde <educaleft@gmail.com>
2021-12-07 07:59:51 +0000

    Añadido el setlocale para español.

--- Bloques/Bloques.cpp
+++ Bloques/Bloques.cpp
@@ -1,24 +1,10 @@
-#include <iostream>
-int main()
-{ // bloque 1, nivel de anidamiento 1
-    std::cout << "Escribe un entero: ";
-    int valor{};
-    std::cin >> valor;
 
-    if (valor > 0)
-    { // bloque 2, nivel de anidamiento 2
-        if ((valor % 2) == 0)
-        { // bloque 3, nivel de anidamiento 3
-            std::cout << valor << " es positivo y par\n";
-        }
-        else
-        { // block 4, also nesting level 3
-            std::cout << valor << " es positivo e impar\n";
-        }
-    }
 
-    return 0;
-}
+
+
+
+
+
 
 
 
@@ -39,9 +25,50 @@ int main()
 
 
 /*#include <iostream>
+int main()
+{ // bloque 1, nivel de anidamiento 1
+    std::cout << "Escribe un entero: ";
+    int valor{};
+    std::cin >> valor;
+
+    if (valor > 0)
+    { // bloque 2, nivel de anidamiento 2
+        if ((valor % 2) == 0)
+        { // bloque 3, nivel de anidamiento 3
+            std::cout << valor << " es positivo y par\n";
+        }
+        else
+        { // block 4, also nesting level 3
+            std::cout << valor << " es positivo e impar\n";
+        }
+    }
+
+    return 0;
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include <iostream>
 
 int main()
 { // comienzo del bloque externo
+    setlocale(LC_ALL, "es_ES.UTF-8");
     std::cout << "Escribe un entero: ";
     int valor{};
     std::cin >> valor;
@@ -58,7 +85,7 @@ int main()
     } // fin de otro bloque anidado
 
     return 0;
-} // fin del bloque externo*/
+} // fin del bloque externo
 
 
 
